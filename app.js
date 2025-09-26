fdc3957be76ce11c35a8d96cc9e19b080e90c379
Dishn293 <kalax1004@gmail.com>
2025-09-26 21:30:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const mondayUpper = document.getElementById('mondayUpper')
 const mondayDown = document.getElementById('mondayDown')
+
 const tuesday = document.getElementById('tuesday')
+
+const wednesdayUpper = document.getElementById('wednesdayUpper')
+const wednesdayDown = document.getElementById('wednesdayDown')
 
 const week = document.getElementById('week')
 
 const now = new Date()
+
 let isUpper = true
 
 function getWeek(date) {
@@ -24,12 +29,18 @@ getWeek(now)
 tuesday.classList.add('bg-success')
 
 if(isUpper === true) {
-    // Для верхней недели
+
     mondayUpper.classList.add('bg-success')
     mondayDown.classList.add('opacity-50')
+    
+    wednesdayUpper.classList.add('bg-success')
+    wednesdayDown.classList.add('opacity-50')
 }
 else {
-    // Для нижней недели
+
     mondayUpper.classList.add('opacity-50')
     mondayDown.classList.add('bg-success')
+    
+    wednesdayUpper.classList.add('opacity-50')
+    wednesdayDown.classList.add('bg-success')
 }
